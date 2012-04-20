3b7a5634a4e9e862cb2549be64a114ff283c4148
Solomon Wang <solomonwang@Solomon-Wangs-MacBook.local>
2012-04-19 17:40:39 -0700

    starting BTFileManager

---
+++ BTMesh/src/com/dic/BTMesh/BTFileManager.java
@@ -0,0 +1,30 @@
+package com.dic.BTMesh;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BTFileManager extends Activity {
+    // Debugging
+    private static final String TAG = "BluetoothFileManager";
+    private static final boolean D = true;
+
+}
